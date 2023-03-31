aa55e14a0db07c264d9d39730cb07b51cf60b368
Franco Sansonetti <franco.sansonetti@edu.unito.it>
2023-03-31 11:18:07 +0200

    Bugfix

--- ricerca.pl
+++ ricerca.pl
@@ -30,9 +30,6 @@ prova(Soluzione) :-
     replace(List,BlankPos,Max,NewList),
     hex_bytes(Hex,NewList),
     fromHexToInteger(Hex,StartingBoard),
-    % assert((iterativeDeepening(StartingBoard,BlankPos, Depth, Solution):-
-    %     write('---- Depth: '), write(Depth),write('\n'),
-    %     nextMove(StartingBoard,Solution,BlankPos, V, Depth))),
     iterativeDeepening(StartingBoard, BlankPos, Depth, Soluzione),
     write(Soluzione).
 
@@ -42,47 +39,27 @@ prova(Soluzione):-
     depth(Depth),
     NewDepth is Depth+1,
     retractall(depth(_)),
-    retractall(nextMove/4),
     assert(depth(NewDepth)),
     prova(Soluzione).
 
 iterativeDeepening(StartingBoard,BlankPos, Depth, Solution):-
     write('---- Depth: '), write(Depth),write('\n'),
-    assertz((nextMove(Position, [], BlankPos, LastMove, MaxDepth):-
-        goal(Solution), 
-        Position == Solution,
-        !,
-        depth(Depth),
-        write('Solution found at depth '), write(Depth),write('!\n'))),
-    assertz((nextMove(Position,[Move|MoveList],BlankPos,LastMove,MaxDepth):-
-        MaxDepth > 0,
-        inverse(Move,Inverse),
-        Inverse \== LastMove,
-        applicabile(Move,BlankPos), 
-        %notMember(Move, [LastMove|PreviousTwoMoves]),
-        trasforma(Position,Move,BlankPos,NewPosition,NewBlankPos),
-        % popAndAppend([LastMove|PreviousTwoMoves],Inverse,LastMoves),
-        NewDepth is MaxDepth-1,
-        nextMove(NewPosition,MoveList,NewBlankPos,Move,NewDepth))),
     nextMove(StartingBoard,Solution,BlankPos, V, Depth).
 
-% nextMove(Position, [], BlankPos, LastMove, MaxDepth):-
-%     goal(Solution), 
-%     Position == Solution,
-%     !,
-%     depth(Depth),
-%     write('Solution found at depth '), write(Depth),write('!\n').
+ nextMove(Position, [], BlankPos, LastMove, MaxDepth):-
+     goal(Solution), 
+     Position == Solution,
+     !,
+     depth(Depth),
+     write('Solution found at depth '), write(Depth),write('!\n').
 
-
-% nextMove(Position,[Move|MoveList],BlankPos,LastMove,MaxDepth):-
-%     MaxDepth > 0,
-%     inverse(Move,Inverse),
-%     Inverse \== LastMove,
-%     applicabile(Move,BlankPos), 
-%     %notMember(Move, [LastMove|PreviousTwoMoves]),
-%     trasforma(Position,Move,BlankPos,NewPosition,NewBlankPos),
-%     % popAndAppend([LastMove|PreviousTwoMoves],Inverse,LastMoves),
-%     NewDepth is MaxDepth-1,
-%     nextMove(NewPosition,MoveList,NewBlankPos,Move,NewDepth).
-
-:-dynamic(nextMove/5).
+ nextMove(Position,[Move|MoveList],BlankPos,LastMove,MaxDepth):-
+     MaxDepth > 0,
+     inverse(Move,Inverse),
+     Inverse \== LastMove,
+     applicabile(Move,BlankPos), 
+     %notMember(Move, [LastMove|PreviousTwoMoves]),
+     trasforma(Position,Move,BlankPos,NewPosition,NewBlankPos),
+     % popAndAppend([LastMove|PreviousTwoMoves],Inverse,LastMoves),
+     NewDepth is MaxDepth-1,
+     nextMove(NewPosition,MoveList,NewBlankPos,Move,NewDepth).
