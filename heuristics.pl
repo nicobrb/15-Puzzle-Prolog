4ef0e2ec54abb697b77de4018ad90ac1a395d080
Mario Bifulco <48624954+TheFlonet@users.noreply.github.com>
2023-04-01 15:06:48 +0200

    Small changes to avoid magic number
    The predicate manhattan has been modified to avoid the use of the magic number 256
    
    There are now 5 arguments to avoid having to always calculate the maximum possible number in the board

--- heuristics.pl
+++ heuristics.pl
@@ -1,21 +1,24 @@
 % predicate wrapper to allow the heuristics to be changed transparently in the search method
-% heuristic(+CurrPos, +EndPos, ?Res)
+% heuristic(+CurrPos, ?Res)
 % CurrPos is the current board in hex format
 heuristic(CurrPos, Res) :-
     manhattan(CurrPos, Res).
 
 manhattan(CurrPos, Res) :-
     board(_, N), Max is N*N,
-    manhattan(CurrPos, Max, N, Res).
-manhattan(_, 0, _, 0).
-manhattan(CurrPos, CellIdx, N, Sum) :-
-    % A 1 is subtracted from CellIdx and CellVal to make the module work 
-    % otherwise with the Max value the algorithm does not return the expected value
-    NextIdx is CellIdx - 1,
-    divmod(NextIdx, N, X2, Y2),
-    % 256 is 2^8, i.e. as many as there are bits to 'shift'.
-    divmod(CurrPos, 256, PartialPos, CellVal),
+    manhattan(CurrPos, Max, N, Max, Res), nodebug.
+manhattan(_, 0, _, _, 0).
+manhattan(CurrPos, CellIdx, N, Dim, Sum) :-
+    CellVal is (CurrPos mod 16),
+    CellVal =\= Dim mod 16, % this mod is needed to generalize to n-puzzle
     Val is CellVal - 1,
     divmod(Val, N, X1, Y1),
-    manhattan(PartialPos, NextIdx, N, PartialSum),
+    NextIdx is CellIdx - 1,
+    divmod(NextIdx, N, X2, Y2),
+    PartialPos is CurrPos >> 8,
+    manhattan(PartialPos, NextIdx, N, Dim, PartialSum),
     Sum is abs(X1-X2) + abs(Y1-Y2) + PartialSum.
+manhattan(CurrPos, CellIdx, N, Dim, Sum) :-
+    NextIdx is CellIdx - 1,
+    PartialPos is CurrPos >> 8,
+    manhattan(PartialPos, NextIdx, N, Dim, Sum).
