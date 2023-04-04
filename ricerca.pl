62e23e911ca4341d3d65fbb7546ed25263a30158
Niciolla <39137998+Niciolla@users.noreply.github.com>
2023-04-04 14:34:54 +0200

    minor bugfix.

--- ricerca.pl
+++ ricerca.pl
@@ -1,4 +1,16 @@
-:-['dominio.pl', 'azioni.pl','./traversal_strategy/iterative_deepening.pl'].
+:-['dominio.pl', 'azioni.pl','./traversal_strategy/iterative_deepening_astar.pl', './traversal_strategy/iterative_deepening.pl'].
+
+prova(Soluzione) :- 
+    is_solvable(X),
+    X = 1,
+    board(List,N),
+    nth0(BlankPos,List,v),
+    Max is N*N,
+    replace(List,BlankPos,Max,NewList),
+    hex_bytes(Hex,NewList),
+    fromHexToInteger(Hex,StartingBoard),
+    ida(StartingBoard, BlankPos, Soluzione),
+    write(Soluzione).
 
 prova(Soluzione) :- 
     is_solvable(X),
@@ -11,5 +23,4 @@ prova(Soluzione) :-
     fromHexToInteger(Hex,StartingBoard),
     iterativeDeepening(StartingBoard, BlankPos, Soluzione),
     write(Soluzione).
-
 
